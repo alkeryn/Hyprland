e03e99a4c71f82ca92c826f209553fc67905c9bb
alkeryn <plbraundev@gmail.com>
2026-01-19 02:13:30 +0100

    Revert "protocols/cm: Fix image description info events (_12781)"
    This reverts commit e0cf88809de12c39ad8a1ad1c0194967b0029ec8.
    as it breaks my hdr

--- src/helpers/Monitor.hpp
+++ src/helpers/Monitor.hpp
@@ -321,8 +321,6 @@ class CMonitor {
     float       minLuminance(float defaultValue = 0);
     int         maxLuminance(int defaultValue = 80);
     int         maxAvgLuminance(int defaultValue = 80);
-    float       maxFALL();
-    float       maxCLL();
 
     bool        wantsWideColor();
     bool        wantsHDR();
@@ -332,13 +330,10 @@ class CMonitor {
     /// Has an active workspace with a real fullscreen window (includes special workspace)
     bool inFullscreenMode();
     /// Get fullscreen window from active or special workspace
-    PHLWINDOW                                                   getFullscreenWindow();
-    std::optional<NColorManagement::PImageDescription>          getFSImageDescription();
+    PHLWINDOW                                          getFullscreenWindow();
+    std::optional<NColorManagement::PImageDescription> getFSImageDescription();
 
-    NColorManagement::SPCPRimaries                              getMasteringPrimaries();
-    NColorManagement::SImageDescription::SPCMasteringLuminances getMasteringLuminances();
-
-    bool                                                        needsCM();
+    bool                                               needsCM();
     /// Can do CM without shader
     bool                                canNoShaderCM();
     bool                                doesNoShaderCM();
